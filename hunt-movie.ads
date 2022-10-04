4f131eb2562d541428a19a1df2aae03de714d31b
chaimalk <108950943+chaimalk@users.noreply.github.com>
2022-10-04 21:28:29 +0200

    Create hunt-movie.ads

---
+++ hunt-movie.ads
@@ -0,0 +1,55 @@
+{
+	"Playstore": {
+
+		"AdsController": {
+
+			"NetworkAds": "admob",
+
+			"BannerAdmob": "ca-app-pub-7448804041969308/8862421823",
+			"InterstitialAdmob": "ca-app-pub-7448804041969308/4072844165",
+
+			"BannerFacebook": "835391227640215_835391620973509",
+			"InterstitialFacebook": "835391227640215_835392087640129",
+			
+			"unityGameID": "4957233",
+			"UnityPlacement": "Interstitial_Android",
+
+			"StartappAds": "209260959",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": true,
+			"Rating": true
+
+		}
+
+	},
+	"Huaweistore": {
+
+		"AdsController": {
+
+			"NetworkAds": "startapp",
+
+			"BannerAdmob": "ca-app-pub-8082149907872048/3898325980",
+			"InterstitialAdmob": "ca-app-pub-8082149907872048/8781442418",
+
+			"BannerFacebook": "1347348165628536_1347349145628438",
+			"InterstitialFacebook": "1347348165628536_1347353132294706",
+
+			"StartappAds": "211203742",
+
+			"Cpa": false,
+			"ImageUrl": "https://i.imgur.com/oWEjQGy.gif",
+			"LinkCpa1": "https://cpbldi.com/911232e",
+			"LinkCpa2": "https://google.com/",
+
+			"watchNow": false,
+			"Rating": false
+
+		}
+
+	}
+}
